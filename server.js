d9d8e37f69a809de02b8570fbf06d03f5fbe2ff4
daniou <danioliveras7@gmail.com>
2024-05-28 22:35:18 +0200

    added CORS

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
+
+// Configuración de CORS
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 // Servir archivos estáticos desde el directorio 'public'
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,7 +26,12 @@ app.get('/', (req, res) => {
 });
 
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+});
 
 let userCount = 0;
 
@@ -27,8 +43,12 @@ io.on('connection', (socket) => {
     socket.on('command', (command) => {
         console.log('Comando recibido:', command);
         
-        // Reenviar el comando a todos los clientes
-        io.emit('command', command);
+        // Reenviar el comando de reproducción o pausa a todos los clientes
+        if (command.action === 'play') {
+            io.emit('playVideo');
+        } else if (command.action === 'pause') {
+            io.emit('pauseVideo');
+        }
     });
 
     socket.on('disconnect', () => {
